d6cae2fea82ef12c46b4b8081eb4c2d57274c196
ravi-ashara <tbs.raviashara@gmail.com>
2021-05-13 15:41:05 +0530

    Show confirm box when click on device back button

--- lib/home.dart
+++ lib/home.dart
@@ -7,119 +7,147 @@ void main() {
   ));
 }
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
+  Home({Key key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  Future<bool> onBackPress(context) {
+    return showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+              title: Text('Do you wish to exit?'),
+              actions: <Widget>[
+                TextButton(
+                  child: Text('Cancel'),
+                  onPressed: () => {Navigator.pop(context, false)},
+                ),
+                TextButton(
+                    onPressed: () => {Navigator.pop(context, true)},
+                    child: Text('Exit'))
+              ],
+            ));
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-            title: Text('Home'),
-            centerTitle: true,
-            backgroundColor: Colors.red[400]),
-        body: new Center(
-            child: SingleChildScrollView(
-                child: Column(children: <Widget>[
-          ElevatedButton(
-            onPressed: () => {Navigator.pushNamed(context, '/list')},
-            child: new Text(
-              'List View With Pull To Refresh',
-            ),
-          ),
-          ElevatedButton(
-            onPressed: () => {Navigator.pushNamed(context, '/lite-roling')},
-            child: new Text(
-              'Lite Roling',
-            ),
-          ),
-          ElevatedButton(
-            onPressed: () => {Navigator.pushNamed(context, '/group-list')},
-            child: new Text(
-              'Group List',
-            ),
-          ),
-          ElevatedButton(
-            onPressed: () => {Navigator.pushNamed(context, '/second')},
-            child: new Text(
-              'Slidable List',
-            ),
-          ),
-          ElevatedButton(
-            onPressed: () => {Navigator.pushNamed(context, '/rating')},
-            child: new Text(
-              'Rating',
-            ),
-          ),
-          ElevatedButton(
-            onPressed: () => {Navigator.pushNamed(context, '/story')},
-            child: new Text(
-              'Story View',
-            ),
-          ),
-          ElevatedButton(
-            onPressed: () => {Navigator.pushNamed(context, '/feature')},
-            child: new Text(
-              'Feature Discovery',
-            ),
-          ),
-          ElevatedButton(
-            onPressed: () => {Navigator.pushNamed(context, '/google-login')},
-            child: new Text(
-              'Google Login',
-            ),
-          ),
-          ElevatedButton(
-            onPressed: () => {
-              Navigator.pushNamed(context, '/pass-data', arguments: {
-                "name": 'Ravi Ashara',
-                "country": 'India',
-              })
-            },
-            child: new Text(
-              'Pass Data',
-            ),
-          ),
-          ElevatedButton(
-            onPressed: () => {Navigator.pushNamed(context, '/device-info')},
-            child: new Text(
-              'Device Info',
-            ),
-          ),
-          ElevatedButton(
-            onPressed: () => {Navigator.pushNamed(context, '/carouser-list')},
-            child: new Text(
-              'Carousel Slider ',
-            ),
-          ),
-          ElevatedButton(
-            onPressed: openUser,
-            child: new Text(
-              'Launch Url ',
-            ),
-          ),
-          ElevatedButton(
-            onPressed: () => {Navigator.pushNamed(context, '/google-map')},
-            child: new Text(
-              'Google Map ',
-            ),
-          ),
-          ElevatedButton(
-            onPressed: () => {Navigator.pushNamed(context, '/news')},
-            child: new Text(
-              'News App',
-            ),
-          ),
-          ElevatedButton(
-            onPressed: () => {Navigator.pushNamed(context, '/camera')},
-            child: new Text(
-              'Upload Image',
-            ),
-          ),
-          ElevatedButton(
-            onPressed: () => {Navigator.pushNamed(context, '/dialog')},
-            child: new Text(
-              'Awesome Dialogs',
-            ),
-          ),
-        ]))));
+    return WillPopScope(
+        onWillPop: () => onBackPress(context),
+        child: Scaffold(
+            appBar: AppBar(
+                title: Text('Home'),
+                centerTitle: true,
+                backgroundColor: Colors.red[400]),
+            body: new Center(
+                child: SingleChildScrollView(
+                    child: Column(children: <Widget>[
+              ElevatedButton(
+                onPressed: () => {Navigator.pushNamed(context, '/list')},
+                child: new Text(
+                  'List View With Pull To Refresh',
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () => {Navigator.pushNamed(context, '/lite-roling')},
+                child: new Text(
+                  'Lite Roling',
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () => {Navigator.pushNamed(context, '/group-list')},
+                child: new Text(
+                  'Group List',
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () => {Navigator.pushNamed(context, '/second')},
+                child: new Text(
+                  'Slidable List',
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () => {Navigator.pushNamed(context, '/rating')},
+                child: new Text(
+                  'Rating',
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () => {Navigator.pushNamed(context, '/story')},
+                child: new Text(
+                  'Story View',
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () => {Navigator.pushNamed(context, '/feature')},
+                child: new Text(
+                  'Feature Discovery',
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () =>
+                    {Navigator.pushNamed(context, '/google-login')},
+                child: new Text(
+                  'Google Login',
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () => {
+                  Navigator.pushNamed(context, '/pass-data', arguments: {
+                    "name": 'Ravi Ashara',
+                    "country": 'India',
+                  })
+                },
+                child: new Text(
+                  'Pass Data',
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () => {Navigator.pushNamed(context, '/device-info')},
+                child: new Text(
+                  'Device Info',
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () =>
+                    {Navigator.pushNamed(context, '/carouser-list')},
+                child: new Text(
+                  'Carousel Slider ',
+                ),
+              ),
+              ElevatedButton(
+                onPressed: openUser,
+                child: new Text(
+                  'Launch Url ',
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () => {Navigator.pushNamed(context, '/google-map')},
+                child: new Text(
+                  'Google Map ',
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () => {Navigator.pushNamed(context, '/news')},
+                child: new Text(
+                  'News App',
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () => {Navigator.pushNamed(context, '/camera')},
+                child: new Text(
+                  'Upload Image',
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () => {Navigator.pushNamed(context, '/dialog')},
+                child: new Text(
+                  'Awesome Dialogs',
+                ),
+              ),
+            ])))));
   }
 
   openUser() async {

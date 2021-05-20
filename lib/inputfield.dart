ec94844b7a10c57b15f42fc89503bdc0b461a5c4
ravi-ashara <tbs.raviashara@gmail.com>
2021-05-20 16:58:37 +0530

    Set form validation

--- lib/inputfield.dart
+++ lib/inputfield.dart
@@ -1,4 +1,6 @@
+import 'package:demo/constants/constant.dart';
 import 'package:flutter/material.dart';
+import 'package:form_field_validator/form_field_validator.dart';
 
 void main() {
   runApp(MaterialApp(home: InputDemo()));
@@ -19,6 +21,7 @@ class _InputDemoState extends State<InputDemo> {
   TextEditingController weburlController = new TextEditingController();
   TextEditingController addressController = new TextEditingController();
   TextEditingController passwordController = new TextEditingController();
+  final _form = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -28,129 +31,153 @@ class _InputDemoState extends State<InputDemo> {
               centerTitle: true,
               backgroundColor: Colors.red[400],
             ),
-            body: SingleChildScrollView(
-                child: Padding(
-                    padding: EdgeInsets.all(20.0),
-                    child: Center(
-                        child: Column(children: <Widget>[
-                      TextField(
-                        autofocus: true,
-                        controller: firstNameController,
-                        decoration: InputDecoration(
-                            border: OutlineInputBorder(),
-                            labelText: 'First Name',
-                            hintText: 'Enter First Name'),
-                        obscureText: false,
-                        style: TextStyle(color: Colors.black87, fontSize: 20),
-                        textInputAction: TextInputAction.next,
-                      ),
-                      SizedBox(height: 15),
-                      TextField(
-                          autofocus: true,
-                          controller: emailController,
-                          decoration: InputDecoration(
-                              prefixIcon: Icon(
-                                Icons.email_outlined,
-                                color: Colors.grey,
-                                size: 14,
-                              ),
-                              floatingLabelBehavior: FloatingLabelBehavior.auto,
-                              border: OutlineInputBorder(),
-                              labelText: 'E-mail',
-                              hintText: 'Enter E-mail'),
-                          keyboardType: TextInputType.emailAddress,
-                          obscureText: false,
-                          style: TextStyle(color: Colors.black87, fontSize: 20),
-                          textInputAction: TextInputAction.next),
-                      SizedBox(height: 15),
-                      TextField(
-                        autofocus: true,
-                        controller: cnoController,
-                        keyboardType: TextInputType.number,
-                        decoration: InputDecoration(
-                            prefixIcon: Icon(
-                              Icons.phone,
-                              color: Colors.grey,
-                              size: 14,
-                            ),
-                            floatingLabelBehavior: FloatingLabelBehavior.auto,
-                            border: OutlineInputBorder(),
-                            labelText: 'Contact Number',
-                            hintText: 'Enter Contact nNumber'),
-                        maxLength: 10,
-                        obscureText: false,
-                        style: TextStyle(color: Colors.black87, fontSize: 20),
-                        textInputAction: TextInputAction.next,
-                      ),
-                      SizedBox(height: 15),
-                      TextField(
-                        autofocus: true,
-                        controller: weburlController,
-                        decoration: InputDecoration(
-                            floatingLabelBehavior: FloatingLabelBehavior.auto,
-                            border: OutlineInputBorder(),
-                            labelText: 'Web Site',
-                            hintText: 'Enter Web URL'),
-                        keyboardType: TextInputType.url,
-                        obscureText: false,
-                        style: TextStyle(color: Colors.black87, fontSize: 20),
-                        textInputAction: TextInputAction.next,
-                      ),
-                      SizedBox(height: 15),
-                      TextField(
-                          autofocus: true,
-                          controller: addressController,
-                          decoration: InputDecoration(
-                              floatingLabelBehavior: FloatingLabelBehavior.auto,
-                              border: OutlineInputBorder(),
-                              labelText: 'Address',
-                              hintText: 'Enter Address'),
-                          keyboardType: TextInputType.streetAddress,
-                          maxLines: 5,
-                          obscureText: false,
-                          style: TextStyle(color: Colors.black87, fontSize: 20),
-                          textInputAction: TextInputAction.newline),
-                      SizedBox(height: 15),
-                      TextField(
-                          autofocus: true,
-                          controller: passwordController,
-                          decoration: InputDecoration(
-                            floatingLabelBehavior: FloatingLabelBehavior.auto,
-                            border: OutlineInputBorder(),
-                            labelText: 'Password',
-                            hintText: 'Enter Password',
-                            suffixIcon: GestureDetector(
-                              onTap: () {
-                                setState(() {
-                                  _showPassword = !_showPassword;
-                                });
-                              },
-                              child: Icon(
-                                _showPassword
-                                    ? Icons.visibility_off
-                                    : Icons.visibility,
-                                color: Colors.grey,
-                                size: 22,
-                              ),
-                            ),
+            body: Form(
+                key: _form,
+                child: SingleChildScrollView(
+                    child: Padding(
+                        padding: EdgeInsets.all(20.0),
+                        child: Center(
+                            child: Column(children: <Widget>[
+                          // TextF
+                          TextFormField(
+                            validator: nameValidator,
+                            autofocus: true,
+                            controller: firstNameController,
+                            decoration: InputDecoration(
+                                border: OutlineInputBorder(),
+                                labelText: 'First Name',
+                                hintText: 'Enter First Name'),
+                            obscureText: false,
+                            style:
+                                TextStyle(color: Colors.black87, fontSize: 20),
+                            textInputAction: TextInputAction.next,
                           ),
-                          obscureText: _showPassword,
-                          style: TextStyle(color: Colors.black87, fontSize: 20),
-                          textInputAction: TextInputAction.send),
-                      SizedBox(height: 15),
-                      ElevatedButton(
-                        onPressed: () {
-                          print(firstNameController.text);
-                          print(emailController.text);
-                          print(cnoController.text);
-                          print(weburlController.text);
-                          print(addressController.text);
-                          print(passwordController.text);
-                        },
-                        child: new Text('Submit'),
-                        style: ElevatedButton.styleFrom(
-                            primary: Colors.purple[400]),
-                      )
-                    ]))))));
+                          SizedBox(height: 15),
+                          TextFormField(
+                              validator: emailValidator,
+                              autofocus: true,
+                              controller: emailController,
+                              decoration: InputDecoration(
+                                  prefixIcon: Icon(
+                                    Icons.email_outlined,
+                                    color: Colors.grey,
+                                    size: 14,
+                                  ),
+                                  floatingLabelBehavior:
+                                      FloatingLabelBehavior.auto,
+                                  border: OutlineInputBorder(),
+                                  labelText: 'E-mail',
+                                  hintText: 'Enter E-mail'),
+                              keyboardType: TextInputType.emailAddress,
+                              obscureText: false,
+                              style: TextStyle(
+                                  color: Colors.black87, fontSize: 20),
+                              textInputAction: TextInputAction.next),
+                          SizedBox(height: 15),
+                          TextFormField(
+                            validator: mobileValidator,
+                            autofocus: true,
+                            controller: cnoController,
+                            keyboardType: TextInputType.number,
+                            decoration: InputDecoration(
+                                prefixIcon: Icon(
+                                  Icons.phone,
+                                  color: Colors.grey,
+                                  size: 14,
+                                ),
+                                floatingLabelBehavior:
+                                    FloatingLabelBehavior.auto,
+                                border: OutlineInputBorder(),
+                                labelText: 'Contact Number',
+                                hintText: 'Enter Contact nNumber'),
+                            maxLength: 10,
+                            obscureText: false,
+                            style:
+                                TextStyle(color: Colors.black87, fontSize: 20),
+                            textInputAction: TextInputAction.next,
+                          ),
+                          SizedBox(height: 15),
+                          TextFormField(
+                            validator: requiredValidator,
+                            autofocus: true,
+                            controller: weburlController,
+                            decoration: InputDecoration(
+                                floatingLabelBehavior:
+                                    FloatingLabelBehavior.auto,
+                                border: OutlineInputBorder(),
+                                labelText: 'Web Site',
+                                hintText: 'Enter Web URL'),
+                            keyboardType: TextInputType.url,
+                            obscureText: false,
+                            style:
+                                TextStyle(color: Colors.black87, fontSize: 20),
+                            textInputAction: TextInputAction.next,
+                          ),
+                          SizedBox(height: 15),
+                          TextFormField(
+                              validator: requiredValidator,
+                              autofocus: true,
+                              controller: addressController,
+                              decoration: InputDecoration(
+                                  floatingLabelBehavior:
+                                      FloatingLabelBehavior.auto,
+                                  border: OutlineInputBorder(),
+                                  labelText: 'Address',
+                                  hintText: 'Enter Address'),
+                              keyboardType: TextInputType.streetAddress,
+                              maxLines: 5,
+                              obscureText: false,
+                              style: TextStyle(
+                                  color: Colors.black87, fontSize: 20),
+                              textInputAction: TextInputAction.newline),
+                          SizedBox(height: 15),
+                          TextFormField(
+                              validator: passwordValidator,
+                              autofocus: true,
+                              controller: passwordController,
+                              decoration: InputDecoration(
+                                floatingLabelBehavior:
+                                    FloatingLabelBehavior.auto,
+                                border: OutlineInputBorder(),
+                                labelText: 'Password',
+                                hintText: 'Enter Password',
+                                suffixIcon: GestureDetector(
+                                  onTap: () {
+                                    setState(() {
+                                      _showPassword = !_showPassword;
+                                    });
+                                  },
+                                  child: Icon(
+                                    _showPassword
+                                        ? Icons.visibility_off
+                                        : Icons.visibility,
+                                    color: Colors.grey,
+                                    size: 22,
+                                  ),
+                                ),
+                              ),
+                              obscureText: _showPassword,
+                              style: TextStyle(
+                                  color: Colors.black87, fontSize: 20),
+                              textInputAction: TextInputAction.send),
+                          SizedBox(height: 15),
+                          ElevatedButton(
+                            onPressed: () {
+                              final isValid = _form.currentState.validate();
+                              if (!isValid) {
+                                return false;
+                              }
+                              print(firstNameController.text);
+                              print(emailController.text);
+                              print(cnoController.text);
+                              print(weburlController.text);
+                              print(addressController.text);
+                              print(passwordController.text);
+                            },
+                            child: new Text('Submit'),
+                            style: ElevatedButton.styleFrom(
+                                primary: Colors.purple[400]),
+                          )
+                        ])))))));
   }
 }

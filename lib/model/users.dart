2337fcceea51e9338b2056c90cb83e9ea2bd6665
ravi-ashara <tbs.raviashara@gmail.com>
2021-05-24 15:16:34 +0530

    Remove unused code

--- lib/model/users.dart
+++ lib/model/users.dart
@@ -7,8 +7,8 @@ import 'dart:convert';
 List<User> usersFromJson(String str) =>
     List<User>.from(json.decode(str).map((x) => User.fromJson(x)));
 
-String usersToJson(List<User> data) =>
-    json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
+// String usersToJson(List<User> data) =>
+//     json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
 
 class User {
   User({

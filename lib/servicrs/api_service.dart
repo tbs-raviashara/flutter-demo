ab306bb5c887b981b175b26743c5471d83e4d74b
ravi-ashara <tbs.raviashara@gmail.com>
2021-05-24 12:33:14 +0530

    Create api service

--- lib/servicrs/api_service.dart
+++ lib/servicrs/api_service.dart
@@ -1,8 +1,10 @@
 import 'dart:convert';
 import 'package:demo/model/article_model.dart';
+import 'package:demo/model/users.dart';
 import 'package:http/http.dart' as http;
 
 class ApiService {
+  static const String userUrl = 'jsonplaceholder.typicode.com';
   final endPointUrl = "newsapi.org";
   final client = http.Client();
   Future<List<Article>> getArticle() async {
@@ -18,5 +20,19 @@ class ApiService {
     List<Article> articles =
         body.map((dynamic item) => Article.fromJson(item)).toList();
     return articles;
+  }
+
+  static Future<List<User>> getUsers() async {
+    try {
+      final response = await http.get(Uri.https(userUrl, '/users'));
+      if (200 == response.statusCode) {
+        final List<User> users = usersFromJson(response.body);
+        return users;
+      } else {
+        return [];
+      }
+    } catch (e) {
+      return [];
+    }
   }
 }

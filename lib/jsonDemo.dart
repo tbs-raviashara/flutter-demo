f6a6a433bf91c5d07419d0b7ba43d938e2cf6ca5
ravi-ashara <tbs.raviashara@gmail.com>
2021-05-24 12:33:02 +0530

    Display json data

---
+++ lib/jsonDemo.dart
@@ -0,0 +1,54 @@
+import 'package:demo/model/users.dart';
+import 'package:demo/servicrs/api_service.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(home: JsonDemo()));
+}
+
+class JsonDemo extends StatefulWidget {
+  JsonDemo({Key key}) : super(key: key);
+
+  @override
+  _JsonDemoState createState() => _JsonDemoState();
+}
+
+class _JsonDemoState extends State<JsonDemo> {
+  List<User> _users;
+  bool loading = false;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    loading = true;
+    ApiService.getUsers().then((value) {
+      setState(() {
+        _users = value;
+        loading = false;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: Scaffold(
+            appBar: AppBar(
+                title: Text(loading ? 'Loading' : 'Users'),
+                backgroundColor: Colors.red[400],
+                centerTitle: true),
+            body: Container(
+                child: ListView.builder(
+                    itemCount: _users == null ? 0 : _users.length,
+                    itemBuilder: (context, index) {
+                      User user = _users[index];
+                      return ListTile(
+                        title: Text(user.name),
+                        subtitle: Text(user.email),
+                        onTap: () {
+                          print(user.toJson());
+                        },
+                      );
+                    }))));
+  }
+}
